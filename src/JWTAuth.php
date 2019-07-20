f03d7f584b502485e88b546316709d1f4c8a1fae
gaodong <1175409005@qq.com>
2019-07-20 16:08:30 +0800

    添加权限检测

--- src/JWTAuth.php
+++ src/JWTAuth.php
@@ -18,19 +18,19 @@ class JWTAuth extends JWT
             return ['code' => -1, 'message' => "参数不能为空"];
         }
         //获取用户标识
-        $identifier = config()['jwt']['identifier'];
-        $key        = config()['jwt']['key'];
+        $identifier = config('jwt.identifier');
+        $key        = config('jwt.key');
         $getUser    = $model->where($identifier, '=', $data[$identifier])->find();
 
         if (empty($getUser)) {
             return ['code' => -1, 'message' => '无法生成token'];
         }
         $token = [
-            'iss'  => config()['jwt']['iss'], //签发者 可选
-            'aud'  => config()['jwt']['aud'], //接收该JWT的一方，可选
-            'iat'  => config()['jwt']['iat'], //签发时间
-            'nbf'  => config()['jwt']['nbf'], //(Not Before)：某个时间点后才能访问，比如设置time+30，表示当前时间30秒后才能使用
-            'exp'  => config()['jwt']['exp'], //过期时间,这里设置2个小时
+            'iss'  => config('jwt.iss'), //签发者 可选
+            'aud'  => config('jwt.aud'), //接收该JWT的一方，可选
+            'iat'  => config('jwt.iat'), //签发时间
+            'nbf'  => config('jwt.nbf'), //(Not Before)：某个时间点后才能访问，比如设置time+30，表示当前时间30秒后才能使用
+            'exp'  => config('jwt.exp'), //过期时间,这里设置2个小时
             'data' => $data
         ];
         return ['code' => 1, 'message' => JWT::encode($token, $key)];
@@ -45,10 +45,10 @@ class JWTAuth extends JWT
     public static function validateToken($token)
     {
 
-        $key = config()['jwt']['key'];
+        $key = config('jwt.key');
         try {
             JWT::$leeway = 60;
-            $tokenInfo   = JWT::decode($token, $key, config()['jwt']['algorithms']);
+            $tokenInfo   = JWT::decode($token, $key, config('jwt.algorithms'));
             return ['code' => 1, 'message' => $tokenInfo];
         } catch (SignatureInvalidException $e) {//签名不正确
             return ['code' => -1, 'message' => $e->getMessage()];
